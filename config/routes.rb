44e13fb328e1f3198e14d535b4fcb8529c334181
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-01-23 20:51:33 +0300

    Add 'about' static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'poem/new'
   post 'poem/create'
   get 'poem/:hash', to: 'poem#show', constraints: { hash: /[-_a-zA-Z0-9=]{1,2000}/ }, as: 'poem_show'
-  get '*other', to: 'poem#error', as: 'error'
+  # get '*other', to: 'poem#error', as: 'error'
 end
